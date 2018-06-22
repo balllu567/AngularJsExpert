842c7a8b19d73563605fa6df3565a5de079fa214
balllu567 <38416711+balllu567@users.noreply.github.com>
2018-06-22 18:31:28 +0530

    gdf

--- searchvideo.js
+++ searchvideo.js
@@ -3,23 +3,25 @@ searchvideoApp.controller('searchvideoController', ['$scope', '$http', 'dataServ
     console.log('Ready for search video');
 
     $scope.searchvideo = 'search Video engine';
-    $scope.datajson = [];
+
 
 
     /*------- API call to get the searcj json data   -------*/
     $scope.data = null;
     dataService.getData().then(function(dataResponse) {
         $scope.data = dataResponse;
+        $scope.datajson = [];
         for (var i = 0; i < $scope.data.data.page["content-items"]["content"].length; i++) {
-            console.log($scope.data.data.page["content-items"]["content"][i]);
+            // console.log($scope.data.data.page["content-items"]["content"][i]);
             $scope.datajson.push($scope.data.data.page["content-items"]["content"][i]);
             angular.forEach($scope.datajson, function(val, ind) {
                 var temp = [];
-                angular.forEach(val, function(val1, ind1) {
-                    if (ind1.match('scene')) {
-                        val1['id'] = val.id
-                        temp.push(val1);
-                    }
+                angular.forEach(val, function(val1, key) { 
+                    $scope.roomNamesObj = { 'poster-image': '', 'name': '' };
+                    if (val1.toLowerCase().match('the')) { name = 'the' } else if (val1.toLowerCase().match('Rear')) { name = 'Rear' } else { name = 'default' }
+
+                    $scope.roomNamesObj = { 'poster-image': val1, 'name': name };
+                    // $scope.roomNames.push($scope.roomNamesObj);
                 });
 
             });
@@ -41,4 +43,18 @@ searchvideoApp.service('dataService', function($http) {
             headers: { 'Authorization': 'Token token=xxxxYYYYZzzz' }
         });
     }
+});
+
+/*-------using filter to get the search pattern-----------*/
+
+searchvideoApp.filter('FilterVideo', function() {
+    return function(input, str) {
+        var tmpvideo = {};
+        angular.forEach(input, function(val, key) {
+            if (val.name.toLowerCase().indexOf(str) !== -1) {
+                tmpvideo[key] = val;
+            }
+        });
+        return tmpvideo;
+    };
 });
